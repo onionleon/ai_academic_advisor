61962a95353ad4235ce4d60eadf9eb427f427bec
Leon <leon.domagalski@me.com>
2024-04-16 23:33:16 +0200

    backend
    changes in the backend

--- backend/models.py
+++ backend/models.py
@@ -4,10 +4,12 @@ class Major(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     major_name = db.Column(db.String(50), unique=True, nullable=False)
     description = db.Column(db.String(500), nullable=False)
+    initials = db.Column(db.String(10))
 
     def to_json(self):
         return {
             "id": self.id,
             "major": self.major_name,
-            "description": self.description
+            "description": self.description,
+            "initials": self.initials
         }
